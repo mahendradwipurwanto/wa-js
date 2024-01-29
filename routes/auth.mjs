80e9955435cc91de6ff55ac18cc60b769ed41a1b
mahendradwipurwanto <mahendradwipurwanto@gmail.com>
2024-01-29 17:26:03 +0700

    fix

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -1,27 +1,30 @@
-import express from "express"
-import db from "../services/mongodb.mjs"
-import {
-    res
-} from "../systems/responseHandler.mjs";
-import {
-    io
-} from "../server.mjs";
+import express from "express";
+import db from "../services/mongodb.mjs";
+import { res } from "../systems/responseHandler.mjs";
+import { io } from "../server.mjs";
 import createSession from "../services/wajs.mjs";
 
-const router = express.Router()
+const router = express.Router();
 
 // Get a list of 50 posts
 router.get("/create-session", async (request, response) => {
-    const providedKey = request.headers['x-api-key']
-    let results = createSession(providedKey);
-    io.emit('order-added', results)
+    try {
+        const providedKey = request.headers['x-api-key'];
+        const results = await createSession(providedKey);
 
-    res.success(response, results)
-})
+        // Emit event after the session is created
+        io.emit('order-added', results);
+
+        res.success(response, results);
+    } catch (error) {
+        console.error(error);
+        res.error(response, 'Error creating session');
+    }
+});
 
 // 404
 router.use('*', function (request, response) {
-    res.notfound(response, 'what???')
-})
+    res.notfound(response, 'what???');
+});
 
-export default router
+export default router;
--- services/wajs.mjs
+++ services/wajs.mjs
@@ -1,176 +1,155 @@
-import db from "../services/mongodb.mjs"
-import whatsapp from "whatsapp-web.js"
-import qrcode from 'qrcode-terminal';
+import db from "../services/mongodb.mjs";
+import whatsapp from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
 
-globalThis.SESSIONS = []
-globalThis.ACTIVE_SESSIONS = {}
+globalThis.SESSIONS = [];
+globalThis.ACTIVE_SESSIONS = {};
 
-const session = {}
+const session = {};
 
-export async function createSession(api_key = null) {
-
-    if(api_key != null) {
-        globalThis.api_key = api_key
-    }
-
-    let collection = await db.collection("devices")
-    console.log(`Creating session for : ${globalThis.api_key}`);
-    const client = new whatsapp.Client({
-        restartOnAuthFail: true,
-        puppeteer: {
-            headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-accelerated-2d-canvas',
-                '--no-first-run',
-                '--no-zygote',
-                '--single-process', // <- this one doesn't works in Windows
-                '--disable-gpu'
-            ],
-        },
-        authStrategy: new whatsapp.LocalAuth({
-            clientId: globalThis.api_key
-        })
-    });
-
-    client.initialize();
-
-    client.on('qr', async (qr) => {
-        qrcode.generate(qr, {
-            small: true
-        });
-        Object.assign(session, {
-            qr: qr
-        });
-
-        const query = {
-            key: globalThis.api_key
+export function createSession(api_key = null) {
+    return new Promise(async (resolve, reject) => {
+        if (api_key != null) {
+            globalThis.api_key = api_key;
         }
 
-        const update = {
-            qr: qr
-        }
-
-        console.log({
-            "process": "qr",
-            "query": query,
-            "update": update
+        let collection = await db.collection("devices");
+        console.log(`Creating session for : ${globalThis.api_key}`);
+        const client = new whatsapp.Client({
+            restartOnAuthFail: true,
+            puppeteer: {
+                headless: true,
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--single-process', // <- this one doesn't works in Windows
+                    '--disable-gpu'
+                ],
+            },
+            authStrategy: new whatsapp.LocalAuth({
+                clientId: globalThis.api_key,
+            }),
         });
 
-        let result = await collection.updateOne(query, {
-            $set: update
-        })
-    });
-
-    client.on('ready', async () => {
-
-        const query = {
-            key: globalThis.api_key
-        }
-
-        const update = {
-            connected: true
-        }
-
-        console.log({
-            "process": "ready",
-            "query": query,
-            "update": update
-        });
-
-        let result = await collection.updateOne(query, {
-            $set: update
-        })
-
-        let dataSession = await collection.findOne({
-            key: globalThis.api_key
-        })
-
-        
-        Object.assign(dataSession, {
-            client: client
-        });
-
-        console.log({
-            "process": "active",
-            "data": dataSession
-        });
-        
-
-        globalThis.ACTIVE_SESSIONS = dataSession
-    });
-
-    client.on('authenticated', () => {
-        return {
-            status: true,
-            message: "Authenticated"
-        };
-    });
-
-    client.on('auth_failure', function () {
-        io.emit('message', {
-            key: key,
-            text: 'Auth failure, restarting...'
-        });
-    });
-
-    client.on('disconnected', async (reason) => {
-        io.emit('message', {
-            key: key,
-            text: 'Whatsapp is disconnected!'
-        });
-        client.destroy();
         client.initialize();
 
-        const query = {
-            key: globalThis.api_key
-        }
+        client.on("qr", async (qr) => {
+            qrcode.generate(qr, {
+                small: true,
+            });
+            Object.assign(session, {
+                qr: qr,
+            });
 
-        const update = {
-            connected: false
-        }
+            const query = {
+                key: globalThis.api_key,
+            };
 
-        console.log({
-            "process": "disconnect",
-            "query": query,
-            "update": update
+            const update = {
+                qr: qr,
+            };
+
+            let result = await collection.updateOne(query, {
+                $set: update,
+            });
+
+            resolve({ status: true, qr: qr });
         });
 
-        let result = await collection.updateOne(query, {
-            $set: update
-        })
-    });
+        client.on("ready", async () => {
+            const query = {
+                key: globalThis.api_key,
+            };
 
+            const update = {
+                connected: true,
+            };
 
-    Object.assign(session, {
-        key: globalThis.api_key,
-        client: client,
-        connected: false
-    });
+            let result = await collection.updateOne(query, {
+                $set: update,
+            });
 
-    // Tambahkan client ke sessions
-    globalThis.SESSIONS.push(session);
+            let dataSession = await collection.findOne({
+                key: globalThis.api_key,
+            });
 
-    console.log({
-        "process": "sessions",
-        "sessions": globalThis.SESSIONS
-    });
+            Object.assign(dataSession, {
+                client: client,
+            });
 
-    const query = {
-        key: globalThis.api_key
-    }
+            globalThis.ACTIVE_SESSIONS = dataSession;
+        });
 
-    let dataSession = await collection.findOne(query)
+        client.on("authenticated", () => {
+            resolve({ status: true, message: "Authenticated" });
+        });
 
-    if (!dataSession) {
+        client.on("auth_failure", function () {
+            io.emit("message", {
+                key: key,
+                text: "Auth failure, restarting...",
+            });
+            reject("Authentication failure");
+        });
 
-        await collection.insertOne({
+        client.on("disconnected", async (reason) => {
+            io.emit("message", {
+                key: key,
+                text: "Whatsapp is disconnected!",
+            });
+            client.destroy();
+            client.initialize();
+
+            const query = {
+                key: globalThis.api_key,
+            };
+
+            const update = {
+                connected: false,
+            };
+
+            console.log({
+                process: "disconnect",
+                query: query,
+                update: update,
+            });
+
+            let result = await collection.updateOne(query, {
+                $set: update,
+            });
+        });
+
+        Object.assign(session, {
             key: globalThis.api_key,
-            connected: false
-        })
-    }
+            client: client,
+            connected: false,
+        });
+
+        // Add client to sessions
+        globalThis.SESSIONS.push(session);
+
+        console.log({
+            process: "sessions",
+            sessions: globalThis.SESSIONS,
+        });
+
+        const query = {
+            key: globalThis.api_key,
+        };
+
+        let dataSession = await collection.findOne(query);
+
+        if (!dataSession) {
+            await collection.insertOne({
+                key: globalThis.api_key,
+                connected: false,
+            });
+        }
+    });
 }
 
-export default createSession
+export default createSession;
